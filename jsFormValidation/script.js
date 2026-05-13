789e327148fb122cc333e7f4255053f22fe65282
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-05-13 18:54:48 +0530

    formValidation

--- jsFormValidation/script.js
+++ jsFormValidation/script.js
@@ -1,24 +1,25 @@
-function submit(){
-    console.log("Submit Button Clicked");
-    const name=document.getElementById("fullName").value;
-    const em= document.getElementById("email").value;
-    const ph= document.getElementById("phone").value;
-    const pass= document.getElementById("password").value;
+function submit() {
+  const fn = document.getElementById("fullName").value;
+  const em = document.getElementById("email").value;
+  const ph = document.getElementById("phone").value;
 
-    if(!/^[A-Za-z\ ]+$/test(name)){
-        document.getElementById("nameError").innerText ="Invalid Full Name"
-    }
-    
-    if(!/^[A-Za-z\.\_\d]+@gmail.com/test(em)){
-        document.getElementById("emailError").innerText ="Invalid Email"
-    }
-    
-    if(!/^[6-9]\d{9}$/.test(ph)){
+  //   document.getElementById("nameError").innerText = "";
+  //   document.getElementById("emailError").innerText = "";
+  //   document.getElementById("phoneError").innerText = "";
 
-    }
-    
-   
-        
-    
-    
-};
+  document.querySelectorAll(".error").forEach((element) => {
+    element.innerText = "";
+  });
+
+  if (!/^[A-Za-z\s]+$/.test(fn)) {
+    document.getElementById("nameError").innerText = "Invalid Full Name";
+  }
+
+  if (!/^[A-Za-z\.\_\d]+@gmail.com$/.test(em)) {
+    document.getElementById("emailError").innerText = "Invalid Email";
+  }
+
+  if (!/^[6-9]\d{9}$/.test(ph)) {
+    document.getElementById("phoneError").innerText = "Invalid Phone";
+  }
+}
