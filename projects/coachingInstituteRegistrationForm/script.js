55d853a1d183757af1bcfddb4cceb763cae99244
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-05-14 20:20:17 +0530

    script

--- projects/coachingInstituteRegistrationForm/script.js
+++ projects/coachingInstituteRegistrationForm/script.js
@@ -4,8 +4,18 @@ document
   .addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fn = document.getElementById("fullName").value;
-    const gndr = document.querySelector("input[name='gender']:checked")?.value;
+
+    const fullName = document.getElementById("fullName").value;
+    const email=document.getElementById("email").value;
+    const phone=document.getElementById("phone").value;
+    const dob=document.getElementById("dob").value;
+    const gender = document.querySelector("input[name='gender']:checked")?.value;
+    const qualification =document.getElementById("qualification").value;
+    const marks=document.getElementById("marks").value;
+    const course=document.getElementById("course").value;
+    const address=document.getElementById("address").value;
+    const city=document.getElementById("city").value;
+    const pin=document.getElementById("pin").value;
 
     const timings = [];
 
@@ -15,10 +25,25 @@ document
         timings.push(item.value);
       });
 
-    console.log(gndr);
 
-    console.log(timings);
+    // console.log(gndr);
+
+    // console.log(timings);
+  console.log("Full Name:", fullname);
+  console.log("Email:", email);
+  console.log("Phone:",phone);
+  console.log("Date of Birth:",dob);
+  console.log("Gender:",gender);
+  console.log("Qualification:",qualification);
+  console.log("Mark:s", marks);
+  console.log("Course:", course);
+  console.log("Timings:",timings);
+  console.log("Address:", address);
+  console.log("City:",city);
+  console.log("Pin Code:",pin);
   });
+
+
 
 // ResetButton
 document
