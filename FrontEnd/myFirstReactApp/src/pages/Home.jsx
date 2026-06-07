24da97ab9fb500f34a2a21a6db0290d621700630
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-06-07 23:08:56 +0530

    home page

--- FrontEnd/myFirstReactApp/src/pages/Home.jsx
+++ FrontEnd/myFirstReactApp/src/pages/Home.jsx
@@ -1,23 +1,29 @@
 import { useNavigate } from "react-router-dom";
-function Home(){
-     return (
+
+function Home() {
+  const navigate = useNavigate();
+  return (
     <>
-      <div className="d-flex justify-content-center">
+      <div className="container d-flex justify-content-center align-items-center">
         <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
-          <h1 className="text-center">Welcome To Home Page!!!</h1>
+          <h1 className="text-center">Welcome to Home Page!!!</h1>
           <p className="text-center">
-            we have a wide range of clothings , eootwear and Accessories for you 
-            Please check our Products and enjoy shopping with us.
+            We have wide range of Chothings, Footwear and Accessories for you.
+            Please check our products and enjoy shopping with us.
           </p>
+
           <div className="d-flex justify-content-center mt-4">
-            <button className="btn btn-primary me-2" >
-                Check our Products
+            <button
+              className="btn btn-primary me-2"
+              onClick={() => navigate("/product")}
+            >
+              Check our Products
             </button>
           </div>
-         
         </div>
       </div>
     </>
   );
 }
+
 export default Home;
