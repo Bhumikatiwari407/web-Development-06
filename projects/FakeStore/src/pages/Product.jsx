817eab6c0674a1b89c1032a23525ad2125cc973c
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-06-10 20:53:37 +0530

    fakestore

---
+++ projects/FakeStore/src/pages/Product.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="p-10 grid grid-cols-4">
+      <div className="w-75 border rounded h-100 ">
+        <div className="w-full h-40">
+          <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png" 
+          alt="" />
+        </div>
+      </div>
+      <span>12</span>
+      <p>12</p>
+      <button className="bg-orange-400 rounded-full px-4 py-2">Add to Cart</button>
+    </div>
+    
+  );
+};
+
+export default Product;
---
+++ projects/cravingExample/src/App.jsx
@@ -0,0 +1,44 @@
+import GridExample from "./components/GridExample";
+
+const App = () => {
+  return (
+    <>
+      <div className="p-3 h-50 bg-linear-to-b from-orange-500 to-white ">
+        something something
+      </div>
+      <GridExample />
+      <div className="bg-[url('/commonbg.png')] bg-cover h-screen w-full flex justify-center items-center">
+        <div className="w-5xl mx-auto border p-20 bg-white/70 rounded-2xl flex flex-col justify-center items-center">
+          <p className="text-green-700">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            culpa consequatur pariatur fuga tempora quam facilis, laboriosam
+            corporis facere consequuntur vero aperiam aspernatur doloribus
+            magnam nam blanditiis commodi ab! Rerum!
+          </p>
+
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="add any text"
+            className="border p-3 placeholder:text-red-600 bg-(--primary-color)"
+          />
+          <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 hover:text-red-600 active:bg-amber-500">
+            submit
+          </button>
+
+          <button className="mybtnDesign">xyz</button>
+
+          <div className="flex items-center gap-3 w-full">
+            <div className="border  border-fuchsia-800 w-full" />
+            <div>some text</div>
+            {/* <div className="border border-fuchsia-800 w-full " /> */}
+            <hr className="border border-fuchsia-800 w-full " />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
