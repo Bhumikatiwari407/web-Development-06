9f6d6b826e2dab6401d9ba603b71b2c2145ed642
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-06-22 20:24:51 +0530

    routers and controllers

--- BackEnd/FirstBackEnd/index.js
+++ BackEnd/FirstBackEnd/index.js
@@ -2,20 +2,45 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import AuthRouter from "./src/routers/auth.route.js"
+import PublicRouter from "./src/routers/public.route.js"
 
 const app = express();
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
 
+//default API
 app.get("/",(req,res)=>{
     console.log("Default Get API Hit")
     res.json({message:"Welcome to my First backend Project"});
 });
 
 
-app.post("/login",(req,res) => {
+// app.post("/login",(req,res) => {
     
-    res.json({message:"Login Successful"});
+//     res.json({message:"Login Successful"});
     
-});
+// });
+// app.get("/logout",(req,res) => {
+    
+//     res.json({message:"Logout Successful"});
+    
+// });
+// app.post("/register",(req,res) => {
+    
+//     res.json({message:"Register Successful"});
+    
+// });
+// app.put("/update",(req,res) => {
+    
+//     res.json({message:"update Successful"});
+    
+// });
+// app.delete("/delete",(req,res) => {
+    
+//     res.json({message:"deleted Successful"});
+    
+// });
 
 const port = process.env.PORT || 5000;
 
