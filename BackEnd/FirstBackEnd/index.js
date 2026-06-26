9cd6eb40b73cac78ea33fb887796f041d80cbc53
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-06-26 20:19:17 +0530

    login

--- BackEnd/FirstBackEnd/index.js
+++ BackEnd/FirstBackEnd/index.js
@@ -1,56 +1,36 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import AuthRouter from "./src/routers/auth.route.js"
-import PublicRouter from "./src/routers/public.route.js"
-import connectDB from "./src/config/dbConnection.config.js"
+import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
+import connectDB from "./src/config/dbConnection.config.js";
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 
-//default API
-app.get("/",(req,res)=>{
-    console.log("Default Get API Hit")
-    res.json({message:"Welcome to my First backend Project"});
+//Default API
+app.get("/", (req, res) => {
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend Project" });
+});
+
+//Default Error Handler
+
+app.use((err, req, res, next) => {
+  const ErrMessage = err.message || "Internal Server Error";
+  const ErrStausCode = err.statusCode || 500;
+
+  res.status(ErrStausCode).json({ message: ErrMessage });
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log("Server Started on port:", port);
-    connectDB();
+app.listen(port, () => {
+  console.log("Server Started on port:", port);
+  connectDB();
 });
-
-
-
-
-
-// app.post("/login",(req,res) => {
-    
-//     res.json({message:"Login Successful"});
-    
-// });
-// app.get("/logout",(req,res) => {
-    
-//     res.json({message:"Logout Successful"});
-    
-// });
-// app.post("/register",(req,res) => {
-    
-//     res.json({message:"Register Successful"});
-    
-// });
-// app.put("/update",(req,res) => {
-    
-//     res.json({message:"update Successful"});
-    
-// });
-// app.delete("/delete",(req,res) => {
-    
-//     res.json({message:"deleted Successful"});
-    
-// });
