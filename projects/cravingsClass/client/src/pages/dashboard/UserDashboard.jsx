647707fc36dbe8f709bea345069c7b351150357f
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-07-02 20:03:27 +0530

    userdashboard

--- projects/cravingsClass/client/src/pages/dashboard/UserDashboard.jsx
+++ projects/cravingsClass/client/src/pages/dashboard/UserDashboard.jsx
@@ -5,7 +5,7 @@ const UserDashboard = () => {
   const { user } = useAuth();
   return (
     <>
-      <div>Welcome Back!! {user.fullName}</div>
+      {/* <div>Welcome Back!! {user.fullName}</div>
       <div>Welcome Back!! {user.email}</div>
       <div>Welcome Back!! {user.phone}</div>
       <div className="w-24 h-24 rounded-full overflow-hidden">
@@ -14,7 +14,13 @@ const UserDashboard = () => {
           alt=""
           className="w-full h-full object-cover"
         />
-      </div>
+      </div> */}
+
+        <div className="flex text-red-600">
+          <div>sidebar</div>
+          <div>content</div>
+        </div>
+
     </>
   );
 };
