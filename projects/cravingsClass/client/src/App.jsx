3c2f8ee471391edee03173cdfa90d65405ae7d28
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-07-09 23:39:04 +0530

    admin dashboard

--- projects/cravingsClass/client/src/App.jsx
+++ projects/cravingsClass/client/src/App.jsx
@@ -8,9 +8,11 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import CustomerDashboard from "./pages/dashboard/CustomerDashboard";
 import Contact from "./pages/ContactUs";
+import RestaurantDashboard from "./pages/dashboard/RestaurantDashboard";
+import RiderDashboard from "./pages/dashboard/RiderDashboard";
+import AdminDashboard from "./pages/dashboard/AdminDashboard";
 
 const App = () => {
-
   return (
     <>
       <Toaster />
@@ -21,10 +23,13 @@ const App = () => {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/register/:userType" elemen t={<Register />} />
+        <Route path="/register/:userType" element={<Register />} />
         <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+        <Route path="/restaurant-dashboard" element={<RestaurantDashboard />} />
+        <Route path="/rider-dashboard" element={<RiderDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
-      <Footer /> 
+      <Footer />
     </>
   );
 };
