6618524a3ff44238d6088ef218e5dc5b542b1acb
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-07-10 00:51:16 +0530

    seeders

--- projects/cravingsClass/client/src/components/customerDashboard/CustomerSetting.jsx
+++ projects/cravingsClass/client/src/components/customerDashboard/CustomerSetting.jsx
@@ -3,8 +3,9 @@ import { MdEdit } from "react-icons/md";
 import { useAuth } from "../../context/AuthContext";
 import api from "../../config/api.config.js";
 import toast from "react-hot-toast";
+import { MdOutlineLockReset } from "react-icons/md";
 import { MdOutlineAddAPhoto } from "react-icons/md";
-import PasswordChangeModal from "../commomModals/PasswordChangeModal";
+import PasswordChangeModal from "../commonModals/PasswordChangeModal";
 
 const CustomerSetting = () => {
   const { user, setUser } = useAuth();
--- projects/cravingsClass/client/src/pages/dashboard/CustomerDashboard.jsx
+++ projects/cravingsClass/client/src/pages/dashboard/CustomerDashboard.jsx
@@ -7,12 +7,12 @@ import { useLocation , useNavigate} from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 const CustomerDashboard = () => {
-  const { isLogin } = useAuth();
+  const { isLogin,role } = useAuth();
   const navigate = useNavigate();
   const active = useLocation().state?.activeTab;
   const [activeTab, setActiveTab] = React.useState(active || "overview");
 
-  if (!isLogin) {
+  if (!isLogin || role !== "customer") {
     return (
       <div className="h-[92vh] bg-[url('/foodTable.webp')]  bg-cover bg-center">
         <div className="h-full backdrop-blur-lg flex flex-col items-center justify-center ">
@@ -34,7 +34,7 @@ const CustomerDashboard = () => {
 
   return (
     <>
-      <div className="h-[92vh] flex gap-2 m-2">
+      <div className="h-[91vh] flex gap-2 p-2">
         <div className="w-3/17 bg-(--color-base-200) p-4 rounded-lg shadow-md h-full">
           <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
         </div>
