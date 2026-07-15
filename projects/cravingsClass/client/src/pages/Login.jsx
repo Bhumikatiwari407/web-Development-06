cf174a52e64bb6bcee8e16f630d1d7c7d2e3eb9e
Bhumikatiwari407 <tiwaribhumi821@gmail.com>
2026-07-15 23:11:11 +0530

    .

--- projects/cravingsClass/client/src/pages/Login.jsx
+++ projects/cravingsClass/client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import React from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
